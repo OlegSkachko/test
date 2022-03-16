55fd5ebbbcf3105e6ac51fc4ff8ce84375e046a1
OlegSkachko <chaegolik@gmail.com>
2022-03-16 20:16:00 +0300

    first commit1

---
+++ .history/src/store/dsr_20220316201531.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const dsr = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default dsr;
---
+++ .history/src/store/dsr_20220316201541.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const dsr = () => {
+    return (
+        <div>
+            jkjl
+        </div>
+    );
+};
+
+export default dsr;
